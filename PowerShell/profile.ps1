94963aa0d6d5f3a06cd8681e87392035c137df26
Farhan Mustar <farhanmustar@gmail.com>
2021-03-06 22:25:06 +0800

    Add vimg alias to start GV in vim on startup.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,3 +1,4 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-Alias ls Get-ChildItemColorFormatWide
 Set-Alias -Force sl Get-ChildItemColorFormatWide
+function vimg { vim -c GV -c tabonly }
