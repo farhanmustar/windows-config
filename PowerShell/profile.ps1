0abe87b16eb2d31e821e43960aec2a8e9bdd1b08
Farhan Mustar <farhanmustar@gmail.com>
2021-03-17 09:30:43 +0800

    Fix grep alias cannot accept pipe input and update alias to use splatting parameters.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,7 +1,10 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-Alias ls Get-ChildItemColorFormatWide
 Set-Alias -Force sl Get-ChildItemColorFormatWide
+Set-Alias open Start-Process
 function vimg { vim -c GV -c tabonly }
-function grep { grep.exe --color=auto $args }
-function ssh { ssh.exe -X -A $args }
-function ssh-basic { ssh.exe -X -A -L 8000:localhost:80 $args }
+function grep { 
+  process { $_ | grep.exe --color=auto @Args }
+}
+function ssh { ssh.exe -X -A @Args }
+function ssh-basic { ssh.exe -X -A -L 8080:localhost:80 @Args }
