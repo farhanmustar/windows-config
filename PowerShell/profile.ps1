89024dd5174bcb8314b0f0a73e28fb5ce84b279a
Farhan Mustar <farhanmustar@gmail.com>
2021-06-11 00:23:30 +0800

    Fix vimod reset tab completion setting.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -3,7 +3,7 @@ Set-Alias ls Get-ChildItemColorFormatWide
 function ll { Get-ChildItem -Attributes Hidden, !Hidden @Args }
 Set-Alias -Force sl Get-ChildItemColorFormatWide
 Set-Alias open Start-Process
-function vimod { Set-PSReadlineOption -EditMode Vi }
+function vimod { Set-PSReadlineOption -EditMode Vi; Set-PSReadlineKeyHandler -Key Tab -Function Complete }
 function vimg { vim -c GV -c tabonly }
 function grep { 
   process { $_ | grep.exe --color=auto @Args }
